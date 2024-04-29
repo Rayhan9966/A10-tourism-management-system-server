2fe5a252362a1a23fae4ea213725cec76894506b
Rayhan9966 <rayhancse162gmail.com>
2024-04-30 00:27:53 +0400

    server connected

--- index.js
+++ index.js
@@ -35,11 +35,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+app.post('/touristspot',async(req,res)=>{
+  const newTouristSpot=req.body;
+  console.log(newTouristSpot);
+})
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
